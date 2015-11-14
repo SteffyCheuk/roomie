3060c2eafd47c069d0bd331d7a7a340f5b4eafee
Mark Yoon <mark.youngho.yoon@gmail.com>
2015-11-14 18:18:15 -0500

    Models and table

--- db.py
+++ db.py
@@ -1,13 +1,44 @@
-# import app
+from app import db
+import datetime
+from sqlalchemy import Column, Integer, String
 
-# class User(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	first_name = db.Column(db.String(100))
-# 	last_name = db.Column(db.String(100))
-# 	email = db.Column(db.String(120), unique=True)
+class User(db.Model):
+    __tablename__ = "users"
 
-# 	def __init__(self, first_name, last_name, email):
-# 		self.first_name = first_name
-# 		self.last_name = last_name
-# 		self.email = email
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(100))
+    last_name = Column(String(100))
+    email = Column(String(100))
+    dob = Column(DateTime)
+    group = Column(Integer)
+    alias = Column(String(100))
 
+class Group(db.Model):
+    __tablename__ = "groups"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+
+class List(db.Model):
+    __tablename__ = "lists"
+
+    id = Column(Integer, primary_key=True)
+    group = Column(Integer)
+    name = Column(String(100))
+    desc = Column(String (500))
+
+class Item(db.Model):
+    __tablename__ = "list_items"
+
+    id = Column(Integer, primary_key=True)
+    list = Column(Integer)
+    desc = Column(String(500))
+
+class Message(db.Model):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True)
+    group = Column(Integer)
+    user = Column(Integer)
+    created_date = Column(DateTime)
+    message = Column(String(500))
