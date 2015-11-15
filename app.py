91d85b7839b52de36f6898f534f4247af0083c9a
Stepheny Cheuk <sc2354@cornell.edu>
2015-11-14 23:18:44 -0500

    implemented some apis

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, make_response, request, Response, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from models import *
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost/roomie'
@@ -8,15 +10,103 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+  return "Hello World!"
 
-@app.route('/log_in')
-def log_in():
-  return "Logging the user in..."
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  email = request.args.get('email')
+  print email
+  session['id'] = '1'
+  return session['id']
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+  return jsonify({'tasks': tasks})
  
+@app.route('/register', methods=['GET', 'POST'])
+def create_account():
+	email = request.args.get('email')	
+	first_name = request.args.get('first_name')
+	last_name = request.args.get('last_name')
+	if email is None or first_name is None or last_name is None:
+		return 'False'
+	else:
+		try:
+			new_user = User(first_name, last_name, email)
+			db.session.add(new_user)
+			db.session.commit()
+		except:
+			return 'False'
+	return 'True'
+
+@app.route('/create_group')
+def create_group():
+	name = request.args.get('name')
+	print name
+	if name is None:
+		return 'False'
+	else:
+		try:
+			# add the new group
+			new_group = Group(name)
+			db.session.add(new_group)
+			db.session.commit()
+			# create corresponding list and update user.group_id
+			new_list = List(new_group.id, 'Groceries', 'A list of household groceries that need to be purchased.')
+			current_user = User.query.get(session['id'])
+			current_user.group_id = new_group.id
+			db.session.merge(current_user)
+			db.session.add(new_list)
+			db.session.commit()
+			return 'True'
+		except:
+			return 'False'
+
+
+@app.route('/add_item')
+def add_item():
+	if session['id'] is None:
+		return 'False'
+	else:
+		list_id = request.args.get('list_id')
+		name = request.args.get('name')
+		quantity = request.args.get('quantity')
+		desc = request.args.get('desc')
+		new_item = ListItem(list_id, name, quantity, desc)
+		if list_id is None or name is None or quantity is None or desc is None:
+			return 'False'
+		else:
+			try:
+				new_item = ListItem(list_id, name, quantity, desc)
+				db.session.add(new_item)
+				db.session.commit()
+				return 'True'
+			except:
+				return 'False'
+
+@app.route('/get_list')
+def get_list():
+	if session['id'] is None:
+		return 'False'
+	else:
+		list_id = request.args.get('list_id')
+		print 'b'
+		items = ListItem.query.filter(ListItem.list_id==list_id).all()
+		print 'i'
+		# items = items.order_by(date_added)
+		print items
+		# build json
+		data = {}
+		for item in items:
+			data[item.id] = {
+				'name': item.name, 
+				'quantity': item.quantity, 
+				'desc': item.desc,
+				'date_added': item.date_added
+			}
+			print data
+		return jsonify(data)
+
 if __name__ == '__main__':
-    app.run()
+	app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+	app.run()
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app import db
+import datetime
 
 class User(db.Model):
     __tablename__ = "users"
@@ -8,7 +9,7 @@ class User(db.Model):
     last_name = db.Column(db.String(100))
     email = db.Column(db.String(100))
     dob = db.Column(db.DateTime)
-    group = db.Column(db.Integer)
+    group_id = db.Column(db.Integer)
     alias = db.Column(db.String(100))
 
     def __init__(self, first_name, last_name, email):
@@ -16,7 +17,8 @@ class User(db.Model):
         self.last_name = last_name
         self.email = email
 
-
+    def __repr__(self):
+        return '<User %r>' % (self.first_name + self.last_name)
 
 class Group(db.Model):
     __tablename__ = "groups"
@@ -27,37 +29,59 @@ class Group(db.Model):
     def __init__(self, name):
         self.name = name
 
+    def __repr__(self):
+        return '<Group %r>' % self.name 
+
 class List(db.Model):
     __tablename__ = "lists"
 
     id = db.Column(db.Integer, primary_key=True)
-    group = db.Column(db.Integer)
+    group_id = db.Column(db.Integer)
     name = db.Column(db.String(100))
     desc = db.Column(db.String (500))
 
-    def __init__(self, name, desc):
+    def __init__(self, group_id, name, desc):
+        self.group_id = group_id
         self.name = name
         self.desc = desc
 
-class Item(db.Model):
+    def __repr__(self):
+        return '<List %r>' % self.name
+
+class ListItem(db.Model):
     __tablename__ = "list_items"
 
     id = db.Column(db.Integer, primary_key=True)
-    list = db.Column(db.Integer)
+    list_id = db.Column(db.Integer)
+    name = db.Column(db.String(100))
+    quantity = db.Column(db.String(100))
     desc = db.Column(db.String(500))
+    date_added = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 
-    def __init__(self, desc):
+    def __init__(self, list_id, name, quantity, desc):
+        self.list_id = list_id
+        self.name = name
+        self.quantity = quantity
         self.desc = desc
+
+    def __repr__(self):
+        return '<Item %r>' % self.name
 
 class Message(db.Model):
     __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True)
-    group = db.Column(db.Integer)
-    user = db.Column(db.Integer)
-    created = db.Column(db.DateTime)
+    group_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
     message = db.Column(db.String(500))
 
-    def __init__(self, message):
+    def __init__(self, group_id, user_id, created_at, message):
+        self.group_id = group_id
+        self.user_id = user_id
+        self.created_at = created_at
         self.message = message
+
+    def __repr__(self):
+        return '<Message %r' % self.message
  
